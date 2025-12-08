2b4d0cc93977f1d3e528b0accdc8813fa739317a
Robert Hidri <robert0hidri@gmail.com>
2025-12-08 19:20:49 +0200

    new idea try

---
+++ js/main.js
@@ -0,0 +1,18 @@
+fetch('assets/data/library.json')
+  .then(res => res.json())
+  .then(data => {
+    const grid = document.querySelector('.grid');
+    data.tiles.forEach(tile => {
+      const div = document.createElement('div');
+      div.className = 'tile';
+      div.textContent = tile.title;
+      div.onclick = () => {
+        if(tile.content.startsWith('http')) {
+          window.open(tile.content, '_blank');
+        } else {
+          window.open(tile.content, '_self'); // placeholder
+        }
+      }
+      grid.appendChild(div);
+    });
+  });
